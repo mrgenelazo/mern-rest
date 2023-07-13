cc6553ed926edd1abe23b43f2f2c1061bdfc3f83
Dennis Genelazo <mr.dennisgenelazo@gmail.com>
2023-07-13 14:26:33 +0800

    Create models

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
-const {errorHandler} = require('./middleware/ErrorMiddleware')
+const {errorHandler} = require('./middleware/ErrorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000
 
+connectDB();
 
 const app = express();
 
